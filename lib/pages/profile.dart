175e389a8db5739be0c38f4315ffccc62a43ae37
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2023-02-04 00:08:01 +0530

    feat: display user info and logout button in profile

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,10 +1,52 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser;
+
+  void signUserOut() {
+    print('Signing out user...');
+    GoogleSignIn().signOut();
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipOval(
+                clipBehavior: Clip.hardEdge,
+                child: Image.network(user!.photoURL!),
+              ),
+              SizedBox(height: 16),
+              Text(
+                user!.displayName ?? 'Anonymous',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16),
+              OutlinedButton.icon(
+                  onPressed: signUserOut,
+                  icon: Icon(Icons.logout),
+                  label: Text('Log Out'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                  )),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
